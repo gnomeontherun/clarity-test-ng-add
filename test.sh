7210de7412c2cfd2da4f135feae06abad6cf4726
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-09-13 12:59:17 -0500

    test multiple versions
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- test.sh
+++ test.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-ng new sample
-cd sample
-if ! ng add @clr/angular
-then
-    echo "Failed to add Clarity"
-    exit 1
-fi
-if ! ng build --prod
-then
-    echo "Failed to build after adding Clarity"
-    exit 1
-fi
+index=0
+
+# We need to test prior to 6.2
+declare -a versions=("6.1.0" "6.2.0")
+for version in "${versions[@]}"
+do
+    # Increment index
+    let "index++"
+    # Install CLI at version
+    npm i -g @angular/cli@${version}
+    # New project
+    ng new sample${index}
+    # Change into it
+    cd sample${index}
+    # Try to install Clarity
+    if ! ng add @clr/angular
+    then
+        echo "Failed to add Clarity"
+        exit 1
+    fi
+    # Try to build prod
+    if ! ng build --prod
+    then
+        echo "Failed to build after adding Clarity"
+        exit 1
+    fi
+    cd ../
+done
