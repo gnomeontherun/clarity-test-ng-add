a5a21c248b574ba514ad2a4d1930fa4a30210d30
Jeremy Wilken <gnomation@gnomeontherun.com>
2019-02-20 13:36:04 -0600

    fix testing
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- test.sh
+++ test.sh
@@ -1,30 +1,20 @@
 #!/usr/bin/env bash
 
-index=0
-
-# We need to test prior to 6.2
-declare -a versions=("6.1.0" "6.2.0")
-for version in "${versions[@]}"
-do
-    # Increment index
-    let "index++"
-    # Install CLI at version
-    npm i -g @angular/cli@${version}
-    # New project
-    ng new sample${index}
-    # Change into it
-    cd sample${index}
-    # Try to install Clarity
-    if ! ng add @clr/angular
-    then
-        echo "Failed to add Clarity"
-        exit 1
-    fi
-    # Try to build prod
-    if ! ng build --prod
-    then
-        echo "Failed to build after adding Clarity"
-        exit 1
-    fi
-    cd ../
-done
+# Install CLI at version
+npm i -g @angular/cli@
+# New project
+ng new sample --defaults=true
+# Change into it
+cd sample
+# Try to install Clarity
+if ! ng add @clr/angular
+then
+    echo "Failed to add Clarity"
+    exit 1
+fi
+# Try to build prod
+if ! ng build --prod
+then
+    echo "Failed to build after adding Clarity"
+    exit 1
+fi
