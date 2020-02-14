166f4324dcba70425a46aa8b32d3223683146ba4
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-02-14 10:20:21 -0600

    chore: update to use circle and test matrix
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- test.sh
+++ test.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Install CLI at version
-npm i -g @angular/cli@
+npm i -g @angular/cli@${1}
 # New project
 ng new sample --defaults=true
 # Change into it
 cd sample
 # Try to install Clarity
-if ! ng add @clr/angular
+if ! ng add @clr/angular@${2}
 then
-    echo "Failed to add Clarity"
+    echo "Failed to add @clr/angular@$1 to @angular/cli@$2"
     exit 1
 fi
 # Try to build prod
 if ! ng build --prod
 then
-    echo "Failed to build after adding Clarity"
+    echo "Failed to build after adding @clr/angular@$1 to @angular/cli@$2"
     exit 1
 fi
